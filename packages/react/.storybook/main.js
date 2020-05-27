54a2a948aab4fddf741a3859c7228d41ef16b6dc
Ryosuke <ryosuke.san.hana@gmail.com>
2020-05-27 09:04:03 -0700

    :sparkles: React Storybook with Docs

---
+++ packages/react/.storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ["../stories/**/*.stories.(js|mdx)"],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-docs",
+  ],
+};
